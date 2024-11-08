94eea805bf5574e1ddca2df0ac99c02db7aa2066
irr-github <ivan.ramirez@pik-potsdam.de>
2024-11-08 18:21:20 +0100

    fix license check export

--- batchslurm.sh
+++ batchslurm.sh
@@ -1,5 +1,4 @@
-#! /bin/bash
-#SBATCH --ntasks 1     
+#! /bin/bash    
 #SBATCH --job-name=Snakemake
 #SBATCH --output=slurmlogs/test-%j.out
 #SBATCH --error=slurmlogs/test-%j.er
@@ -9,8 +8,11 @@
 #SBATCH --qos=priority
 
 PORT=1080
+# sh workflow/scripts/kill-tunnel.sh $PORT
 source /p/system/modulefiles/defaults/piam/module_load_piam
 module load anaconda/2023.09
+echo "Starting license server tunnel"
 sh workflow/scripts/start-license-tunnel.sh $PORT
-snakemake --debug --cores 1 --use-conda
-sh workflow/kill-tunnel.sh $PORT
+gurobi_cl --license &> "gurobi.log"
+# snakemake --debug --cores 1 --use-conda
+sh workflow/scripts/kill-tunnel.sh $PORT
